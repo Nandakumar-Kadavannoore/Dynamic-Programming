174a24bb9fc70e63d3afd73cd37429bc0820fe4b
nandakumar_kadavannoore <nandakumark6693@gmail.com>
2018-06-26 22:45:04 +0530

    The Maximum Increase Product Subsequence
    The Maximum Increase Product Subsequence

---
+++ maximumIncreaseProductSubSequence.java
@@ -0,0 +1,39 @@
+/*
+The Maximum Increasing Product Subsequence
+@author Nandakumar.K
+*/
+public class MainClass {
+	
+	public static void main(String ... args) {
+	    int[] array = new int[5];
+	    array[0] = 1;
+	    array[1] = 4;
+	    array[2] = 5;
+	    array[3] = 2;
+	    array[4] = 6;
+	    System.out.println("The Maximum Increase Product Subsequence length is "+maxProductIS(array, array.length));
+	}
+
+	/*
+	 * Maximum Increasing Product Subsequence
+	 */
+	static int maxProductIS(int array[], int size) {
+		int i,j, max = 0;
+		int MSIS[] = new int [size];
+		
+		for ( i = 0; i < size; i++)
+			MSIS[i] = array[i];
+		
+		for ( i = 1; i < size; i++)
+			for ( j = 0; j < i; j++)
+				if (array[i] > array[j] &&
+						MSIS[i] < MSIS[j] * array[i])
+					MSIS[i] = MSIS[j] * array[i];
+		
+		for ( i = 0 ; i < size; i++)
+			if ( max < MSIS[i])
+				max  = MSIS[i];
+		
+		return max;
+	}
+}
